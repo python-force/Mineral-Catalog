b5827962384e839c7aba310e926ee7e586700615
radek <radek@radeks-MacBook-Pro.local>
2018-10-29 18:03:11 -0400

    django init

---
+++ config/settings/local.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, unicode_literals
+from .common import *
+
+# DEBUG
+# ------------------------------------------------------------------------------
+DEBUG = True
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = 'krr%0&fl+!rd^f!6k&8mxf&as98d7as9a87(*&d-as09_S)AD9'
